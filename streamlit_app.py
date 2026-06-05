82233e861d0bf5fc35f0c2e3fb1a8400fd734da7
JennyHesselbo <rchoffin@gmail.com>
2026-06-05 16:24:48 -0700

    Update streamlit_app.py
    Removed typos of fruitS chosen in row 30 and 31

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string =''
 
-    for fruits_chosen in ingredients_list:
-        ingredients_string += fruits_chosen + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
